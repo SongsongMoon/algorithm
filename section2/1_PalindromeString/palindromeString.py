b25b689f54c1f071c709b704b5a3979226942807
SongsongMoon <kyusong12@gmail.com>
2020-06-25 17:12:02 +0900

    add palindromeString

---
+++ section2/1_PalindromeString/palindromeString.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open("input.txt","rt")
+
+def isPalindrome(s): 
+    return s == s[::-1]
+
+cntCase = int(input())
+
+for i in range(cntCase):
+    string = input()
+    lenStr = len(string)
+
+    for idx in range(lenStr//2):
+        if string[idx].lower() != string[lenStr-1-idx].lower():
+            print("#%d NO" %(i+1))
+            break
+    else:
+        print("#%d YES" %(i+1))
