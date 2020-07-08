d6a4bca9c5a4006673376e5ee44577f2387f13dd
SongsongMoon <kyusong12@gmail.com>
2020-07-08 12:00:47 +0900

    add 2_10

--- section2/10_Sudoku/sudoku.py
+++ section2/10_Sudoku/sudoku.py
@@ -1,19 +1,52 @@
 import sys
 sys.stdin = open("input.txt", "rt")
 
-time = '07:05:45PM'
+def check(arr):
+    for i in range(9):
+        checkRow = [0] * 10
+        checkColumn = [0] * 10
 
-h, m, s = map(int, time[:-2].split(':'))
-print(h,m,s)
-print(time[-2:].upper())
+        for j in range(9):
+            checkRow[arr[i][j]] = 1
+            checkColumn[arr[j][i]] = 1
+        
+        if sum(checkRow) != 9 or sum(checkColumn) != 9:
+            return False
+    
+    for i in range(3):
+        for j in range(3):
+            checkRect = [0] * 10
+
+            for k in range(3):
+                for s in range(3):
+                    checkRect[a[i*3+k][j*3+s]] = 1
+            if sum(checkRect) != 9:
+                return False
+    
+    return True
+
+a = [list(map(int, input().split())) for _ in range(9) ]
+if check(a):
+    print('YES')
+else:
+    print('NO')
+
 """
-grid = [list(map(int, input().split())) for _ in range(9)]
+1. (0,0) ~ (2,2)
+2. (0,3) ~ (2,5)
+3. (0,6) ~ (2,8)
 
-cntSudoku = 1
+4. (3,0) ~ (5,2)
+5. (3,3) ~ (5,5)
+6. (3,6) ~ (5,8)
 
-for i in range(9):
-    for j in range(9):
-        sR = ((cntSudoku%3)-1) * 3
-        sC = (cntSudoku-1) * 3
+7. (6,0) ~ (8,2)
+8. (6.3) ~ (8,5)
+9. (6.6) ~ (8,8)
+
+
+
+for i in range(startRow, endRow):
+    for j in range(startColumn, endColumn):
 
 """
